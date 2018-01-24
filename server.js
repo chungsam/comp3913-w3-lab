f12af66488fb67fa9977420a6a12d9b16e326995
Sam Chung <chungsam@outlook.com>
2018-01-23 21:52:16 -0800

    done

--- server.js
+++ server.js
@@ -1,24 +1,45 @@
 const express = require('express');
-const app = express();
 const fs = require('fs');
 const port = process.env.PORT || 3000;
 
+const app = express();
 
+// Configure ejs as the view engine
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('index', { title: 'hello world yo!' });
+// Server static files
+app.use(express.static('public'));
+app.use('/styles', express.static(__dirname + 'styles'));
+
+// Default route
+app.get('/', (req, res, next) => {
+    res.render('index', { title: 'Week 3 Lab' });
 });
 
-app.get('/api/countries', (req, res) => {
+// API routes
+app.get('/api/countries', (req, res, next) => {
     fs.readFile('./data/ten_most_populated_countries.json', (err, data) => {
         if (err) throw err;
+        let countriesJson = JSON.parse(data);
         res.set('Content-Type', 'application/json');
-        res.send(JSON.parse(data));
-        // res.end();
+        res.send(countriesJson);
     })
 });
 
+app.get('/api/capitals', (req, res, next) => {
+    fs.readFile('./data/canada_capitals.json', (err, data) => {
+        if (err) throw err;
+        let capitalsJson = JSON.parse(data);
+        res.render('capitals', {
+            title: 'Canada Capitals',
+            capitals: capitalsJson
+        });
+    });
+    
+});
+
+
+// Finally, listen on localhost
 app.listen(port, () => {
     console.log(`Server started, listening on port ${port}`);
 });
